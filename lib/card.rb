914b264cbdb681cebf80d71202b4f7b67540a400
Chris M <chris@adadevelopersacademy.org>
2019-03-04 08:13:58 -0800

    Update card.rb

--- lib/card.rb
+++ lib/card.rb
@@ -2,6 +2,16 @@
 # card.rb
 
 class Card
+  attr_reader :value, :suit
+
+  def initialize(value, suit)
+    @value = value
+    @suit = suit
 
 
+  end
+
+  def to_s
+    return "#{value} of #{suit.to_s}"
+  end
 end
