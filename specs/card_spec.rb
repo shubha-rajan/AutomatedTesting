38e6ed8f22e485a3adfc1354a9f3d59c2c910ce9
Shubha Rajan <shubharajan94@gmail.com>
2019-03-04 10:45:41 -0800

    refactored tests

--- specs/card_spec.rb
+++ specs/card_spec.rb
@@ -23,51 +23,49 @@ describe Card do
 
   describe "Testing to_s" do
     it "to_s returns a readable String value logically for values 2-10" do
-      card1 = Card.new(2, :diamonds)
-      card2 = Card.new(4, :hearts)
-      card3 = Card.new(10, :spades)
+      values = [2, 4, 10]
+      suits = [:diamonds, :hearts, :spades]
 
-      expect(card1.to_s).must_equal("2 of diamonds")
-      expect(card2.to_s).must_equal("4 of hearts")
-      expect(card3.to_s).must_equal("10 of spades")
+      expected_results = ["2 of diamonds", "4 of hearts", "10 of spades"]
+
+      values.zip(suits).each_with_index do |param, index|
+        card = Card.new(param[0], param[1])
+        expect(card.to_s).must_equal(expected_results[index])
+      end
     end
 
     it "to_s returns a readable String value for Ace, Jack, Queen, King" do
-      card1 = Card.new(12, :hearts)
-      card2 = Card.new(1, :spades)
-      card3 = Card.new(13, :clubs)
+      values = [12, 1, 13]
+      suits = [:hearts, :spades, :clubs]
 
-      expect(card1.to_s).must_equal("Queen of hearts")
-      expect(card2.to_s).must_equal("Ace of spades")
-      expect(card3.to_s).must_equal("King of clubs")
+      expected_results = ["Queen of hearts", "Ace of spades", "King of clubs"]
+
+      values.zip(suits).each_with_index do |param, index|
+        card = Card.new(param[0], param[1])
+        expect(card.to_s).must_equal(expected_results[index])
+      end
     end
   end
 
   describe "Reader methods" do
     it "Can retrieve the value of the card using a `.value`." do
-      card1 = Card.new(2, :diamonds)
-      card2 = Card.new(4, :hearts)
-      card3 = Card.new(10, :spades)
-      card4 = Card.new(12, :hearts)
-      card5 = Card.new(1, :spades)
-      card6 = Card.new(13, :clubs)
+      values = [2, 4, 10, 12, 1, 13]
+      suits = [:diamonds, :hearts, :spades, :hearts, :spades, :clubs]
 
-      expect(card1.value).must_equal(2)
-      expect(card2.value).must_equal(4)
-      expect(card3.value).must_equal(10)
-      expect(card4.value).must_equal(12)
-      expect(card5.value).must_equal(1)
-      expect(card6.value).must_equal(13)
+      values.zip(suits).each_with_index do |param, index|
+        card = Card.new(param[0], param[1])
+        expect(card.value).must_equal(values[index])
+      end
     end
 
     it "Can retrieve the value of the card using a `.suit`." do
-      card1 = Card.new(2, :diamonds)
-      card2 = Card.new(4, :hearts)
-      card3 = Card.new(10, :spades)
+      values = [2, 4, 10, 12, 1, 13]
+      suits = [:diamonds, :hearts, :spades, :hearts, :spades, :clubs]
 
-      expect(card1.suit).must_equal(:diamonds)
-      expect(card2.suit).must_equal(:hearts)
-      expect(card3.suit).must_equal(:spades)
+      values.zip(suits).each_with_index do |param, index|
+        card = Card.new(param[0], param[1])
+        expect(card.suit).must_equal(suits[index])
+      end
     end
   end
 end
