74826e5752adaa5956424e0487de3b98911c8ae1
Shubha Rajan <shubharajan94@gmail.com>
2019-03-04 10:30:46 -0800

    working tests for Card#initialize and Card#to_s methods

--- lib/card.rb
+++ lib/card.rb
@@ -5,13 +5,25 @@ class Card
   attr_reader :value, :suit
 
   def initialize(value, suit)
+    values = (1..13).to_a
+    suits = [:hearts, :spades, :clubs, :diamonds]
+    if !values.include?(value) || !suits.include?(suit)
+      raise ArgumentError, "Valid suits are :hearts, :spades, :clubs, and :diamonds. " +
+                           "Valid values are integers from 1-13"
+    end
     @value = value
     @suit = suit
-
-
   end
 
   def to_s
-    return "#{value} of #{suit.to_s}"
+    named_values = { 1 => "Ace",
+                     11 => "Jack",
+                     12 => "Queen",
+                     13 => "King" }
+    if named_values.keys.include?(@value)
+      return "#{named_values[@value]} of #{@suit.to_s}"
+    else
+      return "#{@value} of #{@suit.to_s}"
+    end
   end
 end
--- specs/card_spec.rb
+++ specs/card_spec.rb
@@ -1,9 +1,8 @@
 
-require_relative 'spec_helper'
+require_relative "spec_helper"
 
 describe Card do
   describe "You can create a Card instance" do
-
     it "Can be created" do
       [:hearts, :spades, :clubs, :diamonds].each do |suit|
         (1..13).each do |value|
@@ -16,27 +15,35 @@ describe Card do
     it "Raises an ArgumentError for invalid parameters" do
       # To raise an ArgumentError in card.rb
       # raise ArgumentError.new
-      expect {Card.new(0, :diamonds)}.must_raise ArgumentError
-      expect {Card.new(14, :diamonds)}.must_raise ArgumentError
-      expect {Card.new(14, :clovers)}.must_raise ArgumentError
+      expect { Card.new(0, :diamonds) }.must_raise ArgumentError
+      expect { Card.new(14, :diamonds) }.must_raise ArgumentError
+      expect { Card.new(14, :clovers) }.must_raise ArgumentError
     end
   end
 
   describe "Testing to_s" do
-
     it "to_s returns a readable String value logically for values 2-10" do
-      # Test to ensure that to_s works for cards values 2-10
-      # for example:  "2 of diamonds"
+      card1 = Card.new(2, :diamonds)
+      card2 = Card.new(4, :hearts)
+      card3 = Card.new(10, :spades)
+
+      expect(card1.to_s).must_equal("2 of diamonds")
+      expect(card2.to_s).must_equal("4 of hearts")
+      expect(card3.to_s).must_equal("10 of spades")
     end
 
     it "to_s returns a readable String value for Ace, Jack, Queen, King" do
-      # Test to ensure that to_s works for cards values 1, and 11-13
-      # For example: "Queen of hearts"
+      card1 = Card.new(12, :hearts)
+      card2 = Card.new(1, :spades)
+      card3 = Card.new(13, :clubs)
+
+      expect(card1.to_s).must_equal("Queen of hearts")
+      expect(card2.to_s).must_equal("Ace of spades")
+      expect(card3.to_s).must_equal("King of clubs")
     end
   end
 
   describe "Reader methods" do
-
     it "Can retrieve the value of the card using a `.value`." do
       # ensure that `.value works as expected`
     end
@@ -46,5 +53,4 @@ describe Card do
 
     end
   end
-
 end
