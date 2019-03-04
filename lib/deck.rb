40d573b0def162976566a765ee7b0513a5899bcf
Shubha Rajan <shubharajan94@gmail.com>
2019-03-04 11:19:53 -0800

    working methods and tests for Deck class!

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,19 +1,31 @@
 
 # deck.rb
 
-require_relative 'card'
+require_relative "card"
 
 class Deck
+  attr_reader :cards
 
   def initialize
-
+    @cards = []
+    [:hearts, :spades, :clubs, :diamonds].each do |suit|
+      (1..13).each do |value|
+        card = Card.new(value, suit)
+        @cards << card
+      end
+    end
   end
 
   def draw
-    # returns a card
+    drawn_card = @cards.sample
+    @cards.delete(drawn_card)
   end
 
   def shuffle
-    # shuffles the deck
+    @cards.shuffle!
+  end
+
+  def count
+    return @cards.length
   end
 end
--- specs/deck_spec.rb
+++ specs/deck_spec.rb
@@ -1,11 +1,46 @@
 
 
-require_relative 'spec_helper'
+require_relative "spec_helper"
 
 describe Deck do
-  # Write tests for the Deck class.
-  # What should you test?
-    # 1.  Expected Behavior
-    # 2.  Edge cases
+  it "initializes a deck object" do
+    new_deck = Deck.new
+    expect(new_deck).must_be_instance_of Deck
+  end
+  it "contains 52 card objects in an array" do
+    new_deck = Deck.new
 
+    expect(new_deck.cards).must_be_instance_of Array
+    expect(new_deck.cards.length).must_equal 52
+    new_deck.cards.each do |card|
+      expect(card).must_be_instance_of Card
+    end
+  end
+
+  it "contains 52 unique cards" do
+    new_deck = Deck.new
+    expect(new_deck.cards.length).must_equal new_deck.cards.uniq.length
+  end
+  it "has a working shuffle method" do
+    new_deck = Deck.new
+    before_shuffle = new_deck.cards.clone
+
+    new_deck.shuffle
+
+    expect(new_deck.cards).wont_be_same_as(before_shuffle)
+  end
+  it "has a draw method that returns a card and removes it from the deck" do
+    new_deck = Deck.new
+    drawn_card = new_deck.draw
+
+    expect(drawn_card).must_be_instance_of Card
+    expect(new_deck.cards).wont_include (drawn_card)
+  end
+  it "has a count method that returns the number of cards in the deck" do
+    new_deck = Deck.new
+    5.times do
+      expect(new_deck.count).must_equal new_deck.cards.length
+      new_deck.draw
+    end
+  end
 end
